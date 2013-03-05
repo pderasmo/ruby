af57f7bcc795a85d51dfb9713fa2b30dfcc81421
Phil <amiphil_y@yahoo.com>
2013-03-04 20:34:51 -0500

    base working rails "coming soon" sign-up site

---
+++ rails-prelaunch-signup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPrelaunchSignup::Application.config.secret_token = '08066fa8762bb35a5472592573cbc488995c888a5c5f69993a66db3d54531a7649560d8857060d9569aa98162b5639171f31b8cfafd8113bbc6a3ac0c8d3850f'
